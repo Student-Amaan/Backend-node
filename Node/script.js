c642647ead69e906569fd62a67f5f0cf6b749120
waxir <danishdarman06@gmail.com>
2026-04-28 08:45:43 +0500

    backend journey

---
+++ Node/script.js
@@ -0,0 +1,19 @@
+let n = 5;
+
+for(let i = 0; i<=n; i++){
+    console.log("Hello Aman", i)
+}
+
+console.log("bye bye")
+
+
+console.log(process.argv)
+
+let args = process.argv
+
+for(let i=2; i<=args.length; i++){
+    console.log('Hello & welcom', args[i])
+}
+
+const math = require('./math')
+console.log(math.sum(5,4))
