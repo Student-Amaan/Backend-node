dfa4d62ca05dc9befbe1063562cb597d62a0775f
waxir <danishdarman06@gmail.com>
2026-04-29 11:55:24 +0500

    port , get

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,8 +1,29 @@
-const Express = require('express')
-const app = Express()
+const Express = require("express");
+const app = Express();
 
-const port = 8080
+const port = 8080;
 
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
-})
+  console.log(`app listening on port ${port}`);
+});
+
+// res.send({
+//     name: 'Aman',
+//     location: 'southwaziristan',
+//     subloc: 'GhawaKhawa',
+//     port: 8080,
+// })
+
+// app.use((req, res) => {
+//   console.log("request recived...");
+// });
+  app.get("/", (req, res) => {
+    res.send("Your contacted path is");
+  });
+  app.get("/apple", (req, res) => {
+    res.send("Your contacted apple is");
+  });
+  app.get("/orange", (req, res) => {
+    res.send("Your contacted orange is");
+  });
+
