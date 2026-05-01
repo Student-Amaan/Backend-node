c57697cab43612384e0505dc9fd9f18376446953
waxir <danishdarman06@gmail.com>
2026-05-02 00:47:19 +0500

    home,  instagram, files

--- EjsDir/index.js
+++ EjsDir/index.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const port = 3001
 
 app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
 
 app.get('/', (req , res) => {
     res.render("home")
 })
+
+app.get("/ig/:username", (req,res) => {
+    let {username} = req.params;
+    res.render("instagram.ejs", {username})
+})
 app.get('/hello', (req , res) => {
     res.send("home hello")
+})
+app.get('/rolldic', (req , res) => {
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldic.ejs", { diceValue})
 })
 
 app.listen(port, () => {
