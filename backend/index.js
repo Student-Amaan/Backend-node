183672d96d7a05e216d0a875c221c2c1cd89d930
waxir <danishdarman06@gmail.com>
2026-05-03 22:03:14 +0500

    backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 8080;
+
+app.get('/register' , (req , res) => {
+    let {user, password} = req.query;
+    res.send(`thi is GET request! welcome ${user}`)
+})
+app.post('/register' , (req , res) => {
+    res.send("thi is POST request")
+})
+
+app.listen(port, () =>{
+    console.log(`listening to port ${port}`)
+})
