b21157d6dfc24786438a03e07f7740ca963c8de7
waxir <danishdarman06@gmail.com>
2026-05-05 22:29:41 +0500

    Redirect to posts

--- Rest_class/index.js
+++ Rest_class/index.js
@@ -1,44 +1,43 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 const port = 8080;
-const path = require("path")
+const path = require("path");
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", 'ejs')
-app.set("views",path.join(__dirname,'views'))
-app.use(express.static(path.join(__dirname,'public')))
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
-    {
+  {
     username: "aman",
     content: "I love coding",
-    },
-    {
+  },
+  {
     username: "danish",
     content: "I love to make website",
-    },
-    {
+  },
+  {
     username: "waxir",
     content: "I love to lanched project",
-    }
-]
-app.get("/posts", (req , res) => {
-    res.render("index.ejs", {posts});
-})
-app.get("/posts/new", (req , res) => {
-    res.render("new.ejs" )
-})
+  },
+];
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
 
 app.post("/posts", (req, res) => {
-   let {username,content} = req.body
-   posts.push({username,content})
-    res.send("send request is working!")
-})
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
 
 app.listen(port, () => {
-    console.log(`port is listening for ${port}...`)
-})
+  console.log(`port is listening for ${port}...`);
+});
